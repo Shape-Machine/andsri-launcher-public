4f9acc4b22f6bf36a9efe003925066e84ad3840f
Sri Rang <srirangan@gmail.com>
2026-09-19 10:51:02 +0200

    Release andSri 0.8.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "xyz.shapemachine.andsri"
         minSdk = 33
         targetSdk = 36
-        versionCode = 20
-        versionName = "0.8.8"
+        versionCode = 21
+        versionName = "0.8.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
